97daa5ec742383eb84701cccc647f9d0c1849d8c
Trust447 <isaactrust447@gmail.com>
2023-03-23 17:28:03 -0700

    positive_or_negative

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - short description
+ *
+ * Description - long description
+ *
+ *Return 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+
+		printf("%d is negative", i);
+
+	else
+
+		printf("%d is positive", i);
+
+
+}		
