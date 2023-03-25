65bcec59cf1bad7abb6b8d095d04b7f9cab48537
Trust447 <isaactrust447@gmail.com>
2023-03-24 18:17:22 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - uppercase letters
+ * 
+ * @c: char to check
+ *
+ * Return: return 0 or 1
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z'
+			return (1)
+
+			else
+
+			return (0)
+			}
