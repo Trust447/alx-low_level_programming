3b3af63c418abc1ca1dd0b0aad61e5a7c9caaa44
Trust447 <isaactrust447@gmail.com>
2023-04-02 21:29:34 -0700

    kkkk

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,7 +8,7 @@
  * @n: is the number of elements to swap
  *
  * Return: nothing
-*/
+ */
 
 void reverse_array(int *a, int n)
 {
@@ -21,7 +21,7 @@ void reverse_array(int *a, int n)
 	 * then place the last array in
 	 * the first array then place
 	 * value in temp to last array
-	*/
+	 */
 	while (s < e)
 	{
 		temp = a[s];
