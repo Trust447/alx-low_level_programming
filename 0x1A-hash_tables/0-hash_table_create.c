377d3a0fb8c92337a1261bc4d1578c75f0704a63
trust <isaactrust447@gmail.com>
2023-10-06 22:10:09 +0100

    hash htable

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table_created-> = malloc(sizeof(hash_node_t *) * size);
+	table-> = malloc(sizeof(hash_node_t *) * size);
 
 	if (table->array == NULL)
 		return (NULL);
