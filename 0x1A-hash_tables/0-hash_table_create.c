996b473fcc8bf4e82f2796f1f5b29a210283c83d
trust <isaactrust447@gmail.com>
2023-10-06 21:37:23 +0100

    hash htable

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -27,12 +27,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(table); /* Free the previously allocated table */
 		return NULL;
 	}
-	/* Initialize each slot in the array to NULL */
-
-	for (i = 0; i < table->size; i++)
-	{
-		table->array[i] = NULL;
-	}
 
 	return (table);
 }
