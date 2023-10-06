41a61afee5e5af20c63f6b591e55a72a5a138ad0
trust <isaactrust447@gmail.com>
2023-10-06 22:20:37 +0100

    hash htable

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,6 +1,6 @@
 /**
  * hash_djb2 - entry string
- * @str: converted string 
+ * @str: converted string
  * Return: hash of the str
  */
 unsigned long int hash_djb2(const unsigned char *str)
@@ -9,7 +9,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 	int c;
 
 	hash = 5381;
-		        
+
 	while ((c = *str++))
 	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
